d3b3a365ae922f4e87bce877808f555267bec5ff
aidant842 <aidant842@gmail.com>
2020-05-21 08:58:29 +0000

    fixed error no module name fask_pymongo

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-if os.path.exists("env.py"):
-   import env
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+   import env
 
 
 app = Flask(__name__)
