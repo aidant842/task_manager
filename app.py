e014a2f1e4e9a07628371ea9b3266c5bf4a6ad6a
aidant842 <aidant842@gmail.com>
2020-05-21 11:06:48 +0000

    updated requirements.txt

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template('tasks.html', tasks = mongo.db.tasks.find({'complete': {'$ne': 'true'} }))
+    return render_template('tasks.html', tasks = mongo.db.tasks.find({'complete': {'$ne': 'true'}}))
 
 
 @app.route('/add_task')
