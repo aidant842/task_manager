553f73d09916b38532a793088e23f11071232108
aidant842 <aidant842@gmail.com>
2020-05-21 09:01:31 +0000

    tried importing PyMongo again

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
-   import env
+    import env
 
 
 app = Flask(__name__)
